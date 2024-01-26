24f280958fed5457e91eb3b8116b1b49cc8bc516
vivekchaitanya <vivekchaitanya1324@gmail.com>
2024-01-26 14:16:43 +0530

    changes in the package file

--- server/db/connect.js
+++ server/db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const connect = ()=>{
-    mongoose.connect('mongodb://127.0.0.1:27017').then(()=>{
+    mongoose.connect('mongodb://127.0.0.1:27017/taskmanager').then(()=>{
         console.log("Database Connected");
     }).catch((err)=>{
         console.log(err);
